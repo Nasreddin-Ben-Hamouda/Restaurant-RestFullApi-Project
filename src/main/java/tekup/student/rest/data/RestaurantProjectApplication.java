443d4c7037f46e8355e269e4778acfc5672c1176
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2020-12-31 21:29:21 +0100

    empty project

---
+++ src/main/java/tekup/student/rest/data/RestaurantProjectApplication.java
@@ -0,0 +1,13 @@
+package tekup.student.rest.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tekup/student/rest/data/RestaurantProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tekup.student.rest.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
